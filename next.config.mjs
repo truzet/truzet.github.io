334fe7322186f8b0443c93c91970523b0854e361
truzet <mohamedhamed_99@icloud.com>
2026-02-09 02:39:10 +0200

    fix: use relative asset paths for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '',
+  assetPrefix: '.',
   images: {
     unoptimized: true,
   },
+  trailingSlash: false,
 };
 
 export default nextConfig;
